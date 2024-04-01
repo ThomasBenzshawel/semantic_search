8e2b1d33f8cbec3a2d0119cd70cc52c171bb0b60
Thomas Benzshawel <benzshawelt@msoe.edu>
2024-04-01 15:26:33 -0500

    Initial Commit Set Up with data

---
+++ model_structure/utils.py
@@ -0,0 +1,41 @@
+from __future__ import unicode_literals, print_function, division
+from io import open
+import torch
+
+from model_structure.model import EOS_token
+from model_structure.model import SOS_token
+
+
+class Tokenize:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Count SOS and EOS
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+def tensorFromSentence(lang, sentence, device):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(1, -1)
+
+def tensorsFromPair(pair, input, output):
+    input_tensor = tensorFromSentence(input, pair[0])
+    target_tensor = tensorFromSentence(output, pair[1])
+    return (input_tensor, target_tensor)
